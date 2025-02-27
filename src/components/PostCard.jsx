32197fd0a64ebf4935583caa2b28767209fddc82
nicolamarino <nicolamarino527@gmail.com>
2025-02-27 18:04:46 +0100

    [feat] add postlist map of postcard

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,27 +1,29 @@
 export default function PostCard() {
     return (
+        <div>
+            test
+        </div>
 
-
-        <div className='bg'>
-            <h2 className='posts-title'>Lista dei Post</h2>
-            <div className="post-list mt-5">
-                {posts.map((post) => (
-                    <Link to={`/posts/${post.id}`} className='card-link'>
-                        <div className="card" key={post.id}>
-                            <img
-                                src={post.image}
-                                className="card-img-top"
-                                alt="Card image"
-                            />
-                            <div className="card-body">
-                                <h5 className="card-title">{post.title}</h5>
-                                <p className="card-text">{post.tags}</p>
-                                <p className="card-text">{post.content}</p>
-                            </div>
-                        </div>
-                    </Link>
-                ))}
-            </div>
-        </div >
+        // <div className='bg'>
+        //     <h2 className='posts-title'>Lista dei Post</h2>
+        //     <div className="post-list mt-5">
+        //         {posts.map((post) => (
+        //             <Link to={`/posts/${post.id}`} className='card-link'>
+        //                 <div className="card" key={post.id}>
+        //                     <img
+        //                         src={post.image}
+        //                         className="card-img-top"
+        //                         alt="Card image"
+        //                     />
+        //                     <div className="card-body">
+        //                         <h5 className="card-title">{post.title}</h5>
+        //                         <p className="card-text">{post.tags}</p>
+        //                         <p className="card-text">{post.content}</p>
+        //                     </div>
+        //                 </div>
+        //             </Link>
+        //         ))}
+        //     </div>
+        // </div >
     );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,30 +1,12 @@
 import { useState, useEffect } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import axios from 'axios';
-import { usePost } from '../contexts/PostContext';
+import { usePosts } from '../contexts/PostContext';
+import PostCard from './PostCard';
 
 export default function PostList() {
-    // stato per i post
-    const [posts, setPosts] = useState([]);
-
-    // stato per l errore
-    const [errorMessage, setErrorMessage] = useState('');
-
-    // Funzione per ottenere i post
-    function fetchPosts() {
-        axios.get('http://localhost:3000/posts')
-            .then((res) => {
-                setPosts(res.data);
-            })
-            .catch((error) => {
-                console.error("Errore nella chiamata API:", error);
-            });
-    }
-
-    // Carica i post quando il componente si monta
-    useEffect(() => {
-        fetchPosts();
-    }, []);
+    // prendiamo i post dal context
+    const { posts } = usePosts()
 
     return (
 
@@ -33,32 +15,11 @@ export default function PostList() {
             <h2 className='posts-title'>Lista dei Post</h2>
             <div className="post-list mt-5">
                 {posts.map((post) => (
-                    <Link to={`/posts/${post.id}`} className='card-link'>
-                        <div className="card" key={post.id}>
-                            <img
-                                src={post.image}
-                                className="card-img-top"
-                                alt="Card image"
-                            />
-                            <div className="card-body">
-                                <h5 className="card-title">{post.title}</h5>
-                                <p className="card-text">{post.tags}</p>
-                                <p className="card-text">{post.content}</p>
-                            </div>
-                        </div>
-                    </Link>
+                    // cicliamo i post e passiamo la prop a postcard
+                    <PostCard key={post.id} post={post} />
                 ))}
             </div>
         </div >
     );
-
-    // <>
-    //     {posts.map((post) => (
-    //         <PostCard />
-    //     ))
-
-    //     }
-
-    // </>
 
 }
